13494199f9e942dc3517fd70d9c8ea9ca27d3ab6
Giacomo Lawrance <giacomolawrance@gmail.com>
2023-10-04 12:02:32 +0100

    auto insert into existing sheet
    #3 TODO: paste numbers as numbers - currently the numbers are pasted as text, which causes issues with formula on other sheets
    #4 TODO: prevent first row (headers) being copied on spreadsheet for cleaner insertion
    #5 TODO: dynamic sheet insertion depending on attachment file name (sales vs quotes)

--- sort.py
+++ sort.py
@@ -1,6 +1,12 @@
 import win32com.client
 import os
 import pandas as pd
+import openpyxl
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+# TODO: paste numbers as numbers - currently the numbers are pasted as text, which causes issues with formula on other sheets
+# TODO: prevent first row (headers) being copied on spreadsheet for cleaner insertion
+# TODO: dynamic sheet insertion depending on attachment file name (sales vs quotes)
 
 # outlook object creation
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
@@ -36,6 +42,28 @@ for email in emails:
             df.to_clipboard(index=False, header=True, sep='\t')
 
             print(f'Successfully sorted and copied to the clipboard with columns.')
+
+            # open existing excel file
+            excel_file_path = 'C:\\Users\\marketing\\Documents\\2023 SALES FOLLOW UP.xlsx'
+            wb = openpyxl.load_workbook(excel_file_path)
+            
+            # specify sheet
+            sheet_name = 'Sales Order Data'
+            ws = wb[sheet_name]
+
+            # clear existing data apart from first row and later columns
+            for row_index, row in enumerate(ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=19), 2):
+                for cell_index, cell in enumerate(row, 1):
+                    if cell.column_letter not in ['T', 'U', 'V']:
+                        cell.value = None
+
+            # insert sorted data
+            for row_index, row_data in enumerate(dataframe_to_rows(df, index=False, header=True), 2):
+                for column_index, value in enumerate(row_data, 1):
+                    ws.cell(row=row_index, column=column_index, value=value)
+
+            wb.save(excel_file_path)
+            wb.close()
 
             os.remove(attachment_filename)
         else:
