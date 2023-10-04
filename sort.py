2627da2d8c82e6333bc9092e034b8f552635a195
Giacomo Lawrance <giacomolawrance@gmail.com>
2023-10-04 13:47:25 +0100

    format dates correctly, better insertion
    closes #4

--- sort.py
+++ sort.py
@@ -5,7 +5,6 @@ import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows
 
 # TODO: paste numbers as numbers - currently the numbers are pasted as text, which causes issues with formula on other sheets
-# TODO: prevent first row (headers) being copied on spreadsheet for cleaner insertion
 # TODO: dynamic sheet insertion depending on attachment file name (sales vs quotes)
 
 # outlook object creation
@@ -22,7 +21,6 @@ for email in emails:
     # define email subject to search for
     if email.Subject == "CSV file from Report Wizard":
         subject = email.Subject
-        print(f"Subject: {subject}")
 
         # attachment check
         if email.Attachments.Count > 0:
@@ -35,37 +33,46 @@ for email in emails:
             # add to dataframe with windows encoding
             df = pd.read_csv(attachment_filename, encoding='cp1252')
 
-            # sort criteria
+            # convert dates
+            df['Date Entered'] = pd.to_datetime(df['Date Entered'], format='%d/%m/%y').dt.strftime('%d/%m/%Y')
+            df['Date Promised'] = pd.to_datetime(df['Date Promised'], format='%d/%m/%y').dt.strftime('%d/%m/%Y')
+            df['Date Despatched'] = pd.to_datetime(df['Date Despatched'], format='%d/%m/%y').dt.strftime('%d/%m/%Y')
+
+            # Sort criteria
             df.sort_values(by=['Order No', 'Order Line', 'Date Entered'], inplace=True)
 
-            # copy dataframe to clipboard
-            df.to_clipboard(index=False, header=True, sep='\t')
+            df.to_clipboard(index=False, header=True, sep='\t', decimal=',')
 
-            print(f'Successfully sorted and copied to the clipboard with columns.')
+            print(f'Sorted.')
 
-            # open existing excel file
+            # Open the Excel file
             excel_file_path = 'C:\\Users\\marketing\\Documents\\2023 SALES FOLLOW UP.xlsx'
             wb = openpyxl.load_workbook(excel_file_path)
-            
-            # specify sheet
+            print('File opened.')
+
+            # Specify the sheet name to overwrite the data
             sheet_name = 'Sales Order Data'
             ws = wb[sheet_name]
+            print('Sheet selected.')
 
-            # clear existing data apart from first row and later columns
+            # Clear existing data in the sheet, excluding columns T, U, and V, starting from row 2
             for row_index, row in enumerate(ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=1, max_col=19), 2):
                 for cell_index, cell in enumerate(row, 1):
                     if cell.column_letter not in ['T', 'U', 'V']:
                         cell.value = None
-
+            print('Data cleared, inserting.')
+            
             # insert sorted data
-            for row_index, row_data in enumerate(dataframe_to_rows(df, index=False, header=True), 2):
+            for row_index, row_data in enumerate(dataframe_to_rows(df, index=False, header=True), 1):
                 for column_index, value in enumerate(row_data, 1):
                     ws.cell(row=row_index, column=column_index, value=value)
 
+            # Save the updated Excel file
             wb.save(excel_file_path)
             wb.close()
 
             os.remove(attachment_filename)
+            print('Inserted and cleaned. Complete. B)')
         else:
             print("No attachments found in the email.")
 
